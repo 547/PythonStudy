c903c84064af804667fbefa1c7de290e1522b575
qiwang <timer_sevenwang@163.com>
2017-12-11 18:16:27 +0800

    update

--- FirstTest.py
+++ FirstTest.py
@@ -305,10 +305,13 @@ from book import Info, Author
 #     print item.string # 网游之天谴修罗
 
 
+
 #MARK: UI
 from Tkinter import *
 def tapbutton():
     print "Test"
+    #整个程序退出并销毁
+    window.quit()
 window = Tk() # 创建窗口
 table = Listbox(master=window) # 创建tableView
 data = ["001","002","003","004","005"]
@@ -316,8 +319,7 @@ for item in data:
     # 给tableView 数据源
     table.insert(0, item)
 table.pack() # 将小部件放置到主窗口中
-button = Button(master=window, text = "Test", command = tapbutton, width = 100, state = NORMAL) #tapbutton()只调用一次, tapbutton就会一直调用
+button = Button(master=window, text = "Quit", command = tapbutton, width = 100, state = NORMAL) #tapbutton()只调用一次, tapbutton就会一直调用
 button.pack()
 window.mainloop() # 进入消息循环 窗口展示出来
-
 
